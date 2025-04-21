19bec42cfe23de3b9321635d3a4abaf51bbac9c2
Juankpr0 <camitrianasuarez@gmail.com>
2025-04-21 15:04:01 -0500

    21-04

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./Estructura/header/header.component";
+import { FooterComponent } from './Estructura/footer/footer.component';
+import { HomeComponent } from "./P\u00E1ginas/home/home.component";
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Trinity';
 }
+
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
-import { Routes } from '@angular/router';
+import { Routes, RouterOutlet} from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { HomeComponent } from './Páginas/home/home.component';
+import { RegistrosComponent } from './Páginas/registros/registros.component';
+import { StockComponent } from './Páginas/stock/stock.component';
+import { UsersComponent } from './Páginas/users/users.component';
+import { NoFoundComponent } from './Páginas/no-found/no-found.component';
+import { AlertasComponent } from './Páginas/alertas/alertas.component';
+import { DetallesComponent } from './Páginas/alertas/detalles/detalles.component';
+import { OtrosComponent } from './Páginas/otros/otros.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'home', component: HomeComponent},
+    {path: 'stock', component: StockComponent},
+    {path: 'registros', component: RegistrosComponent},
+    {path: 'users', component: UsersComponent},
+    {path: 'alertas', component: AlertasComponent},
+    {path: 'detalles', component: DetallesComponent},
+    {path: 'otros', component: OtrosComponent},
+    {path: '**', component: NoFoundComponent}
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+  })
+  export class AppRoutingModule {}
